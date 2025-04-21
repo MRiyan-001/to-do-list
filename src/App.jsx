3113606eeb8f61544e7f8d1e17af3f67055446e9
MRiyan-001 <mrh.world001@gmail.com>
2025-04-21 09:27:25 +0500

    Path errors are resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./components/main";
+import Main from "./components/Main";
 import "./index.css";
 
 function App() {
